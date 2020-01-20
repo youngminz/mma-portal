560e59d1599666acfd580dd249d2fe62cbc7239d
Youngmin Koo <youngminz.kr@gmail.com>
2020-01-21 00:36:52 +0900

    feat: Celery Beat 실행 시 마이그레이션 수행

--- scripts/start_celery_beat.sh
+++ scripts/start_celery_beat.sh
@@ -4,4 +4,5 @@ set -ex
 
 export DJANGO_SETTINGS_MODULE=mma_portal.settings.production
 aws ssm get-parameter --name /mma_portal/secrets --query 'Parameter.Value' --output text --region ap-northeast-2 > .env
+python manage.py migrate
 celery beat -A mma_portal -l info
